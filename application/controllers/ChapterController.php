1c919251b08149e9b44033b2192fd1e5e61bcc45
Didik <didik@localhost>
2020-05-20 10:10:16 +0700

    àdd chapter api

---
+++ application/controllers/ChapterController.php
@@ -0,0 +1,58 @@
+<?php
+    require APPPATH . 'libraries/REST_Controller.php';
+
+    class ChapterController extends REST_Controller {
+
+        public function __construct() {
+            parent::__construct();
+            $this->load->model('chapterDao');
+        }
+
+        public function chapters_get($mangaId){
+            $chaptersList = $this->chapterDao->getByMangaId($mangaId);
+            $chapters = array();
+            foreach($chapterList as $chapter) {
+                $tmp = array(
+                    "id" => $chapter->id,
+                    "number" => $chapter->number, 
+                    "title" => $chapter->title, 
+                    "retrieve_date" => $chapter->retrieve_date, 
+                    "release_date" => $chapter->release_date
+                );
+                array_push($chapters, $tmp);
+            }
+            $responseBody = array(
+                'chapters' => $chapters
+            );
+
+            $this->response(array(
+                'status' => 'success', 
+                'message' => 'Success', 
+                'data' => $responseBody)
+            );
+        }
+
+        public function chapterDetail_get($chapterId){
+            $imageList = $this->chapterDao->getByChapterId($chapterId);
+            $chapterImages = array();
+            foreach($imageList as $chapterImage) {
+                $tmp = array(
+                    "id" => $chapter->id,
+                    "image_url" => $chapter->image_url, 
+                    "width" => $chapter->width, 
+                    "height" => $chapter->height
+                );
+                array_push($chapterImages, $tmp);
+            }
+            $responseBody = array(
+                'images' => $chapterImages
+            );
+
+            $this->response(array(
+                'status' => 'success', 
+                'message' => 'Success', 
+                'data' => $responseBody)
+            );
+        }
+    }
+?>
