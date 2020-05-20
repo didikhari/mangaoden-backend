3b41eac72b2808424d74bb319ad44292a1b112fd
Didik <didik@localhost>
2020-05-20 14:42:30 +0700

    fix error getById

--- application/models/SourceDao.php
+++ application/models/SourceDao.php
@@ -3,7 +3,7 @@
     
     class SourceDao extends CI_Model{
         
-        public function getId($mangaId) {
+        public function getById($mangaId) {
             $query = $this->db->get_where('source', array('id' => $mangaId));
             return $query->row_array();
         }
