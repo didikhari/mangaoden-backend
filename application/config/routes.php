235e8c63078965e1252869beaca84273ba9b59f7
didikhari <didikhari38@gmail.com>
2020-07-28 16:02:38 +0700

    add solo leveling

--- application/config/routes.php
+++ application/config/routes.php
@@ -82,3 +82,4 @@ $route['retriever/leviatanscans/chronicles-of-heavenly-demon'] = 'AnyScansRetrie
 $route['retriever/mobpsycho100-manga/mob-psycho-100'] = 'ComicEaselRetriever/manga/17/23';
 $route['retriever/kimetsu-noyaiba/demon-slayer'] = 'ComicEaselRetriever/manga/18/38';
 $route['retriever/boruto-naruto/boruto'] = 'ComicEaselRetriever/manga/19/15';
+$route['retriever/sololeveling-manga/sololeveling'] = 'ComicEaselRetriever/manga/20/22';
