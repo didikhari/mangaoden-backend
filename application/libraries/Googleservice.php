bca1b711bce33e8641d35229d1229ad7cae90df8
didikhari <didikhari38@gmail.com>
2020-08-02 00:52:40 +0700

    add test

--- application/libraries/Googleservice.php
+++ application/libraries/Googleservice.php
@@ -2,6 +2,22 @@
     defined('BASEPATH') OR exit('No direct script access allowed');
 
     class Googleservice {
+
+        public function test() {
+            $client = new Google_Client();
+            $client->setAuthConfig('.assets/client_secrets.json');
+            $client->addScope(Google_Service_Drive::DRIVE_METADATA_READONLY);
+            if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
+                $client->setAccessToken($_SESSION['access_token']);
+                $drive = new Google_Service_Drive($client);
+                $files = $drive->files->listFiles(array())->getItems();
+                echo json_encode($files);
+            } else {
+                $redirect_uri = 'https://crawl.didikhari.web.id/index.php/driveauth';
+                header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
+            }
+        }
+        
         public function createSubFolder($parentFolderId, $folderName){
             log_message('info', 'creating sub folder');
             $client = $this->getClient();
